cc55cab186eaa970ed095936124a205089963d9d
Gilbert Kibet <kibetgilly354@gmail.com>
2023-01-26 20:41:59 +0300

    Add project files.

---
+++ MathLibrary/Math.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public class Math
+    {
+        public int SumNumbers(int a,int b)
+        {
+            var total = a + b;
+            return total;
+        }
+    }
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace MyApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(   Gilbert.SumNumbers(12, 12));
+
+            StringBuilder sb = new StringBuilder("Gilbert");
+
+            sb.Append('7');
+
+            sb.ToString();
+
+            Console.WriteLine(sb);
+        }
+    }
+
+    //public static class Gilbert
+    //{
+    //    public static int a;
+
+    //    public static int b;
+
+    //    public static int SumNumbers(int a,int b)
+    //    {
+    //        int total = a + b;
+
+    //        return total;
+    //    }
+    //}
+
+
+}
